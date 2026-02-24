435817d976e83ee59bb9f58d76d520bc0e721b3f
Shiv7722 <chouhanshivraj836@gmail.com>
2026-02-24 10:28:24 +0530

    folder name changes

---
+++ backend/src/models/Note.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+//1. Create a Schema
+//2. Create a Model based on the Schema
+
+const noteSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }, //createdAt //updatedAt
+);
+
+const Note = mongoose.model("Note", noteSchema);
+export default Note
