e9c25ecc81331d2a5a002203c5ec050121c4ef93
Shiv7722 <chouhanshivraj836@gmail.com>
2026-02-24 17:57:56 +0530

    toaster axios added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,22 @@
 import { useState } from "react";
 import {Routes,Route} from "react-router";
+import { Toaster } from "react-hot-toast";
+
 import HomePage from "./pages/HomePage";
 import CreateNote from "./pages/CreateNote";
 import NoteInfo from "./pages/NoteInfo";
 
 function App() {
   return (
-    <>
+    <div className="relative h-full w-full">
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
       <Routes>
         <Route path="/" element={<HomePage/>} />
-        
+        <Route path="/create" element={<CreateNote/>}/>
+        <Route path="/note/:id" element={<NoteInfo/>}/>
       </Routes>
-    </>
+      <Toaster />
+    </div>
   )
 }
 
